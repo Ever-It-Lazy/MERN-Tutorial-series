4b961a959d0bd65381e890ba499319d6624cfc67
Everett Lindsay <everettsjunk@gmail.com>
2022-12-04 00:49:57 -0500

    update works

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { getNotes } = require("../controllers/noteController");
+const { getNotes, createNote, getNoteById, updateNote } = require("../controllers/noteController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route('/').get(protect, getNotes);
-// router.route('/create').post();
-// router.route('/:id').get().put().delete();
+router.route('/create').post(protect, createNote);
+router.route('/:id')
+    .get(getNoteById)
+    .put(protect, updateNote).delete();
 
 module.exports = router;
