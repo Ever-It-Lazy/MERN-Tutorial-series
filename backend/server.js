406c6df18d2f5221b00b0f2444f376f6f48c0f6e
Everett Lindsay <everettsjunk@gmail.com>
2022-12-03 20:31:02 -0500

    get /api/notes isn't working

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
@@ -14,11 +15,12 @@ app.get('/', (req,res) => {
     res.send("API is running..");
 });
 
-app.get('/api/notes', (req,res) => {
-    res.json(notes);
-});
+// app.get('/api/notes', (req,res) => {
+//     res.json(notes);
+// });
 
 app.use('/api/users', userRoutes);
+app.get('/api/notes', noteRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
