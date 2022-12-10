ea22a38df02c4791439bf59190ce5ac474794c42
Everett Lindsay <everettsjunk@gmail.com>
2022-12-09 20:51:01 -0500

    make it better

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,13 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const path = require("path");
 
 const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get('/', (req,res) => {
-	res.send("API is running..");
-});
 
 // app.get('/api/notes', (req,res) => {
 // 	res.json(notes);
@@ -21,6 +19,24 @@ app.get('/', (req,res) => {
 
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+
+// --------------- deployment ---------------
+
+// __dirname = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+// 	app.get("*", (req,res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+// 	});
+// } else {
+// 	app.get('/', (req,res) => {
+// 		res.send("API is running..");
+// 	});
+// }
+
+// --------------- deployment ---------------
+
 
 app.use(notFound);
 app.use(errorHandler);
